e4b289e449535de0e57024bf645f0ac05643aa2e
Knowledgeseeker2022 <117414151+Knowledgeseeker2022@users.noreply.github.com>
2026-06-08 18:39:42 +0000

    Minor changes to clean up Mock Data

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  devIndicators: false,
   serverExternalPackages: ["@prisma/client", "bcryptjs"],
   images: {
     remotePatterns: [
